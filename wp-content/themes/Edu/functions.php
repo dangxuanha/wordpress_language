18042be379821133768cb0becd88be6d53d38895
Dang Ha <dangha@Dangs-Mac-mini.local>
2015-05-19 17:10:32 +0700

    version 1.0

---
+++ wp-content/themes/Edu/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author SmoothThemes - http://www.SmoothThemes.com
+ * @copyright 2013
+ */
+
+
+// Remove if debug
+ error_reporting(0);
+/**
+ * Call stFramework
+ */
+require_once('st-framework/st-framework.php');
+require_once('includes/theme-init.php');
+
+/**
+ * Write any functions here if you are using child-theme.( You should use child-theme for future update )
+ */
